4c80bbcb8b38580d25f38c663e4270e10eae9c5f
StationaryStation <raulytstation@outlook.com>
2022-12-25 14:51:01 -0400

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/searchR/",
+  base: "/SearchR/",
   plugins: [svelte()],
 })
